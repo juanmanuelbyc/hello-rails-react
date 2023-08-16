5fd432f64fe5761ca60abfa5f1e69ae7affe2dc8
Juan Diaz <jfdiaza@unal.edu.co>
2023-08-16 05:13:37 -0400

    set-up-rails-react-app

---
+++ app/javascript/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Greeting from './Greeting'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Welcome!</div>,
+  },
+  {
+    path: '/greeting',
+    element: <Greeting />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+}
+
+export default App
